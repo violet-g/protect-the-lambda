1aae1ee4796f03ff6ef35b2a83140a5e5dee805a
violet-g <gabriella.georgieva17@gmail.com>
2017-12-29 19:49:54 +0200

    Add init files

---
+++ src/Tafl/Interact.hs
@@ -0,0 +1,36 @@
+{- |
+
+This module defines the games two modes of interactions: REPL and REPL.
+
+-}
+module Tafl.Interact
+  ( repl
+  ) where
+
+import System.Exit
+import System.IO
+
+import Tafl.Core
+import Tafl.Process
+
+
+-- | Core REPL for processing user actions.
+repl :: GameState -- ^ The initial starting start of the game
+     -> IO ()
+repl st = do
+    hSetBuffering stdout NoBuffering
+    hSetBuffering stdin  LineBuffering
+    doREPL st
+
+  where
+    doREPL :: GameState -> IO ()
+    doREPL st = do
+      putStr "tafl> "
+      raw_cmd <- getLine
+      result  <- processCommandStr st raw_cmd
+      case result of
+        (Left err) -> do
+          printError err
+          repl st
+
+        (Right newSt) -> repl newSt
